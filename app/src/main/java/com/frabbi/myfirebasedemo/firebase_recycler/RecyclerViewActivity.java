9f25f893c7658787766ccd9e67c2483d91144994
mfrrabbi <mdfazlerabbi.diu@gmail.com>
2021-11-27 12:22:27 +0600

    Firebase_with_search

--- app/src/main/java/com/frabbi/myfirebasedemo/firebase_recycler/RecyclerViewActivity.java
+++ app/src/main/java/com/frabbi/myfirebasedemo/firebase_recycler/RecyclerViewActivity.java
@@ -1,19 +1,25 @@
 package com.frabbi.myfirebasedemo.firebase_recycler;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SearchView;
 
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.frabbi.myfirebasedemo.R;
 import com.frabbi.myfirebasedemo.adapters.MyRecyclerAdapter;
 import com.frabbi.myfirebasedemo.base.BaseActivity;
 import com.frabbi.myfirebasedemo.databinding.ActivityRecyclerViewBinding;
 import com.frabbi.myfirebasedemo.model.ContactModel;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 public class RecyclerViewActivity extends BaseActivity<ActivityRecyclerViewBinding> {
     private MyRecyclerAdapter myAdapter;
+
     @Override
     protected ActivityRecyclerViewBinding onCreateViewBind(Bundle savedInstanceState) {
         return ActivityRecyclerViewBinding.inflate(getLayoutInflater());
@@ -29,7 +35,6 @@ public class RecyclerViewActivity extends BaseActivity<ActivityRecyclerViewBindi
         myAdapter = new MyRecyclerAdapter(options);
         mBind.recViewContainer.setLayoutManager(new LinearLayoutManager(this));
         mBind.recViewContainer.setAdapter(myAdapter);
-
     }
 
     @Override
@@ -43,4 +48,40 @@ public class RecyclerViewActivity extends BaseActivity<ActivityRecyclerViewBindi
         super.onStop();
         myAdapter.stopListening();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.search_menu_item, menu);
+        MenuItem item = menu.findItem(R.id.app_bar_search);
+        SearchView searchView = (SearchView) item.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                processSearch(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                processSearch(newText);
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void processSearch(String str) {
+        Query query = FirebaseDatabase.getInstance().getReference().child("Contact")
+                .orderByChild("name").startAt(str).endAt(str + "\uf8ff");
+        FirebaseRecyclerOptions<ContactModel> options =
+                new FirebaseRecyclerOptions.Builder<ContactModel>()
+                        .setQuery(query, ContactModel.class)
+                        .build();
+        myAdapter = new MyRecyclerAdapter(options);
+        myAdapter.startListening();
+        mBind.recViewContainer.setAdapter(myAdapter);
+    }
+
+
 }
